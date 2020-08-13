61e37f6bd95b1d2d7a3327f51a8dd737accf9a0a
hmyu <hy155>
2020-08-14 11:37:08 +1200

    change HomeCamera.java to butterknife edition

--- app/src/main/java/com/apple/xhs/custom_view/HomeCamera.java
+++ app/src/main/java/com/apple/xhs/custom_view/HomeCamera.java
@@ -2,12 +2,13 @@ package com.apple.xhs.custom_view;
 
 
 import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
-import android.provider.MediaStore;
+
 import android.view.View;
-import android.widget.Toast;
+import android.widget.ImageView;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
@@ -16,51 +17,50 @@ import com.apple.xhs.MainActivity;
 import com.apple.xhs.R;
 import com.base.BaseActivity;
 
+import butterknife.BindView;
 
 public class HomeCamera extends BaseActivity implements View.OnClickListener {
-    static final int REQUEST_IMAGE_CAPTURE = 1;
-    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
-    @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.home_camera);
-        initView();
-    }
+
+    @BindView(R.id.home_close_camera)
+    ImageView home_close_camera;
+    @BindView(R.id.home_video)
+    ImageView home_video;
+    @BindView(R.id.home_note)ImageView home_note;
 
     @Override
     public int getContentViewId() {
         return R.layout.home_camera;
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        initView();
+    }
+
     @SuppressLint("ResourceAsColor")
     private void initView() {
 
-        View note = findViewById(R.id.home_video);
-        note.setOnClickListener(this);
-        View picture = findViewById(R.id.take_picture);
-        picture.setOnClickListener(this);
-    }
+        home_close_camera.setOnClickListener(this);
+        home_video.setOnClickListener(this);
+        home_note.setOnClickListener(this);
 
-    public void backToMainpage(View view) {
-        finish();
+
     }
 
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
+            case R.id.home_close_camera:
+                Intent home_close_camera = new Intent(HomeCamera.this, MainActivity.class);
+                startActivity(home_close_camera);
+                break;
             case R.id.home_video:
-                startActivity(new Intent(HomeCamera.this, NoteEdit.class));
+            case R.id.home_note:
+                Intent home_video = new Intent(HomeCamera.this, NoteEdit.class);
+                startActivity(home_video);
                 break;
-            case R.id.take_picture:
-                dispatchTakePictureIntent();
-                break;
-        }
-    }
-
-    private void dispatchTakePictureIntent() {
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
         }
     }
 }
